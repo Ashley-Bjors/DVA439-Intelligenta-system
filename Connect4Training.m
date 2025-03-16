2783af7141a63f39b3d0a5740687bbf437859be7
DanRyten <daniel.rytenberg@gmail.com>
2025-03-16 10:07:53 +0100

    added if for selecting whether to start from scratch

--- Connect4Training.m
+++ Connect4Training.m
@@ -1,16 +1,19 @@
 clear all
 
+startNew = true;
+if(startNew)
+    %   If you want to start from scratch
+    agent = load(".\basicAgent.mat").agent;
+    agentMatrix = agent;
+    save("agentMatrix.mat","agentMatrix");
+else
+    %   If you dont want to start from scratch
+    agentMatrix = load(".\agentMatrix")
+    agent = agentMatrix(size(agentMatrix,2))
+end
+
+
 env = createConnect4Env();
-
-%   If you want to start from scratch
-agent = load(".\basicAgent.mat").agent;
-agentMatrix(1) = agent;
-save("agentMatrix.mat","agentMatrix");
-
-%   If you dont want to start from scratch
-%agentMatrix = load(".\agentMatrix")
-%agent = agentMatrix(size(agentMatrix,2))
-
 for i = 1:3
     %Trains the newest agent
     agent = mytrain(agent,env);
