63e20f9f0675200360f9969a998a71f0340e3a4a
DanRyten <daniel.rytenberg@gmail.com>
2025-03-18 11:06:12 +0100

    changed verify

--- verifyConnect4.m
+++ verifyConnect4.m
@@ -2,25 +2,38 @@ function winRate = verifyConnect4()
 
 
 agentMatrix = load(".\agentMatrix").agentMatrix;
-winRate(1:size(agentMatrix,2)) = 0;
+winRate(size(agentMatrix,2):size(agentMatrix,2)) = 0;
 
-triesAgainstSame = 10;
-
-for i = 1:size(agentMatrix,2)
-    for j = 1:triesAgainstSame
-        env = Connect4Env(); % Skapa en instans av spelet
-        state = env.reset(); % Starta om spelet
-        
-
-        while ~env.isDone
-            %   Get action from opponent
-            move = getAction(agentMatrix(i),state);
-            move = cell2mat(move);
-            %   Get response from agent
-            [state, reward, env.isDone] = env.step(move);
+triesAgainstSame = 1;
+for k = 1:size(agentMatrix,2)
+    for i = 1:size(agentMatrix,2)
+        for j = 1:triesAgainstSame
+            env = Connect4Env(); % Skapa en instans av spelet
+            state = env.reset(); % Starta om spelet
+            
+    
+            while ~env.isDone
+                %   get action from agent
+                move = getAction(agentMatrix(size(agentMatrix,2)),state);
+                move = cell2mat(move);
+                %   Get response from agent
+                [state, reward, env.isDone] = env.step(move);
+                
+                if(env.isDone)
+                    break;
+                end
+                
+                %   Get action from opponent
+                move = getAction(agentMatrix(i),state);
+                move = cell2mat(move);
+                %   Get response from agent
+                [state, reward, env.isDone] = env.step(move);
+                
+            end
+            env.displayBoard();
+           
+            winRate(i) = winRate(i) - env.player/triesAgainstSame;
+            
         end
-       
-        winRate(i) = winRate(i) - env.player/triesAgainstSame;
-        
     end
 end
