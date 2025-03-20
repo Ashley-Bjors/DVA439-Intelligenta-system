cd188a5555e39fd53023b89c7bfb55bbbab1af4f
DanRyten <daniel.rytenberg@gmail.com>
2025-03-20 16:01:41 +0100

    allow for playing against an agent

--- testConnect4.m
+++ testConnect4.m
@@ -2,13 +2,36 @@ env = Connect4Env(); % Skapa en instans av spelet
 state = env.reset(); % Starta om spelet
 env.displayBoard();
 
-while ~env.isDone
-    move = input('Ange ett kolumnnummer (1-7): '); % Spelarens input
-    [nextState, reward, isDone] = env.step(move);
+
+agentMatrix = load(".\agentMatrix").agentMatrix;
+agent = agentMatrix(130);
+
+whoIsFirst = 1;
+if(whoIsFirst == 1)
+    whoIsFirst = 1;
+else
+    whoIsFirst = -1;
+end
+playAgainstAgent = 1;
+playAgain = 1;
+while playAgain
+    env = Connect4Env(); % Skapa en instans av spelet
+    state = env.reset(); % Starta om spelet
     env.displayBoard();
-    
-    if isDone
-        disp('Spelet är slut!');
-        break;
+    while ~env.isDone
+        if(env.player*whoIsFirst == 1 || playAgainstAgent == 0)
+            move = input('Ange ett kolumnnummer (1-7): '); % Spelarens input
+        else
+            move = getAction(agent,state);
+            move = cell2mat(move);
+        end
+        [state, reward, env.isDone] = env.step(move);
+        env.displayBoard();
+        
+        if env.isDone
+            disp('Spelet är slut!');
+            break;
+        end
     end
+    playAgain = input("Ange 1 för att spela igen och ett annat nummer för att sluta: ");
 end
