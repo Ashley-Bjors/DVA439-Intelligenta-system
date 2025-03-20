5f18d415a741c0c64f2264ef230ee8b9e7842637
DanRyten <daniel.rytenberg@gmail.com>
2025-03-20 16:01:58 +0100

    partial prallelliasion

--- verifyConnect4.m
+++ verifyConnect4.m
@@ -4,9 +4,9 @@ function winRate = verifyConnect4()
 agentMatrix = load(".\agentMatrix").agentMatrix;
 winRate(size(agentMatrix,2),size(agentMatrix,2)) = 0;
 
-
-for i = 1:size(agentMatrix,2)
-    for j = 1:size(agentMatrix,2)
+sizeOfMatrix = size(agentMatrix,2);
+parfor i = 1:sizeOfMatrix
+    for j = 1:sizeOfMatrix
         if(i ~= j)
             env = Connect4Env(); % Skapa en instans av spelet
             state = env.reset(); % Starta om spelet
@@ -30,17 +30,16 @@ for i = 1:size(agentMatrix,2)
                 [state, reward, env.isDone] = env.step(move);
                 
             end
-            env.displayBoard();
            
             winRate(i,j) = env.player; %-1 means the i beat j and a 1 means j beat i
         end
     end
 end
-performance(size(agentMatrix,2)) = 0;
+performance(sizeOfMatrix) = 0;
 hold off
 figure(1)
-for i = 1:size(agentMatrix,2)
-    for j = 1:size(agentMatrix,2)
+for i = 1:sizeOfMatrix
+    for j = 1:sizeOfMatrix
         if(winRate(i,j) == -1)
             scatter(i,j,"+","red")
             performance(i) = performance(i) + 1;
@@ -63,8 +62,8 @@ bar(performance)
 title("Summed performance per agent")
 
 result = 0;
-for i = 1:size(agentMatrix,2)
-    for j = 1:size(agentMatrix,2)
+parfor i = 1:sizeOfMatrix
+    for j = 1:sizeOfMatrix
         if(i > j) % If we are in the upper right side
             if(winRate(i,j) > 0)
                 result = result + 1;
